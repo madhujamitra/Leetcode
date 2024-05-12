c1a694432c65940260631d577518dcfc8d143575
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-05-11 18:01:22 -0700

    Time: 52 ms (58.58%) | Memory: 48.4 MB (86.66%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+// haystack = haystack.split("");
+// needle = needle.split("")
+   //loop one ths to check 
+// let i = 0;
+//    while(i < haystack.length){
+//     if(haystack[i] === needle[0]){
+//         j++;
+//         // for(let j = 0; j<needle.length ;j++){
+
+//         // }
+
+//     }
+//    }
+
+const val = haystack.indexOf(needle)
+return val
+
+};
