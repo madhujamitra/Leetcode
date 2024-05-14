3f7aa5abca259c38ab4417d11a85a2bd88ef04b7
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-05-14 12:35:39 -0700

    Time: 199 ms (5.07%) | Memory: 58.8 MB (21.62%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,13 +3,9 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    var reverse = 0;
-    var copy = x;
-     while (copy > 0) {
-      const digit = copy % 10;
-      reverse = reverse * 10 + digit;
-      copy = ~~(copy / 10);
-    }
-
-    return reverse == x;
+    const rev = x.toString().split("").reverse().join();
+    console.log(rev)
+const val = x.toString().split("").join();
+console.log(val)
+    return val === rev
 };
