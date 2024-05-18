3fff8bc16f407bce4b474cc66fe8a0c3a1c8b1e3
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-05-18 12:32:43 -0700

    Time: 68 ms (57.43%) | Memory: 53.6 MB (38.54%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.js
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.js
@@ -5,22 +5,13 @@
  */
 var sumIndicesWithKSetBits = function (nums, k) {
     let l = nums.length
-    let arr = binaryArr(l);
     let total = 0
     for (let i = 0; i < l; i++) {
-        if (arr[i] === k)
+        let val = i.toString(2).split('1').length - 1
+        if (val === k)
             total = total + nums[i]
     }
 
     return total
 };
 
-function binaryArr(l) {
-    let newArr = []
-    for (let i = 0; i < l; i++) {
-        let val = i.toString(2).split('1').length - 1
-        newArr.push(val)
-    }
-
-    return newArr
-}
