ee1abf141894e222a0a37b7b0c4b0a73f6f38073
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-05-17 17:08:49 -0700

    Time: 51 ms (66.52%) | Memory: 48.8 MB (73.82%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.js
+++ 1791-richest-customer-wealth/richest-customer-wealth.js
@@ -2,15 +2,13 @@
  * @param {number[][]} accounts
  * @return {number}
  */
-var maximumWealth = function(accounts) {
- let newArr = accounts.map(x => addtion(x))
- //console.log
-// addtion(accounts[])
-  return Math.max(...newArr)
+var maximumWealth = function (accounts) {
+    let newArr = accounts.map(x => addtion(x))
+    return Math.max(...newArr)
 };
 
-function addtion(arr){
-let sum = arr.reduce((a, b) => a + b, 0)
+function addtion(arr) {
+    let sum = arr.reduce((a, b) => a + b, 0)
     return sum
 
 }
