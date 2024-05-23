bc1a2f016e89b044d5254f3d38764b1a115bbb22
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-05-23 13:56:16 -0700

    Time: 104 ms (9.63%) | Memory: 55.1 MB (10.58%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function (nums) {
+    nums.sort((a, b) => a - b)
+    console.log(nums)
+    let arr = []
+    let newArr = [...nums]
+
+    for (let i = 0; i < nums.length; i+1) {
+       if(newArr[i] !== undefined){
+         let Al = newArr.shift()
+        let bob = newArr.shift()
+        arr.push(bob, Al)
+       } else {
+        break
+       }
+
+    }
+
+    return arr
+};
