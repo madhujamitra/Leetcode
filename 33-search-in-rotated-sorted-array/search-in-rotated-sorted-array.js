861fbadd27c44af0c981c84977601e034abb4c28
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-09-06 11:52:14 -0700

    Time: 50 ms (71.83%) | Memory: 48.8 MB (79.22%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,8 +4,6 @@
  * @return {number}
  */
 var search = function (nums, target) {
-    if (!nums.includes(target)) return -1
     const condition = (element) => element === target;
-  return nums.findIndex(condition);
-
+    return nums.findIndex(condition);
 };
