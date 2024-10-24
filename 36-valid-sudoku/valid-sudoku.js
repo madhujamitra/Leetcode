b1958eeadec8ad78737e5fd107c0b4e1a302b232
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-10-23 17:03:08 -0700

    Time: 13 ms (94.03%) | Memory: 56 MB (11.37%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -2,31 +2,65 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-  for (let i = 0; i < 9; i++) {
-    let row = new Set(),
-        col = new Set(),
-        box = new Set();
-
+var isValidSudoku = function (board) {
+   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      let _row = board[i][j];
-      let _col = board[j][i];
-      let _box = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
-      
-      if (_row != '.') {
-        if (row.has(_row)) return false;
-        row.add(_row);
-      }
-      if (_col != '.') {
-        if (col.has(_col)) return false;
-        col.add(_col);
-      }
-      
-      if (_box != '.') {
-        if (box.has(_box)) return false;
-        box.add(_box);
-      } 
+            if (!(rowChecker(board, i, j) && columnChecker(board, i, j) && boxChecker(board, i, j))) {
+                return false
+            }
+        }
+
     }
-  }
-  return true
+    return true
+
 };
+
+function rowChecker(board, row, col) {
+    let rowSet = new Set()
+    for (let i = 0; i < 9; i++) {
+        if (board[i][col] !== '.') {
+            if (!rowSet.has(board[i][col]) && (board[i][col] >= 1 && board[i][col] <= 9)) {
+                rowSet.add(board[i][col])
+            } else {
+                return false
+            }
+        }
+
+    }
+    return true
+}
+
+function columnChecker(board, row, coloum) {
+    let colSet = new Set()
+    for (let i = 0; i < 9; i++) {
+        if (board[row][i] !== '.') {
+            if (!colSet.has(board[row][i]) && (board[row][i] >= 1 && board[row][i] <= 9)) {
+                colSet.add(board[row][i])
+            } else {
+                return false
+            }
+        }
+
+    }
+    return true
+}
+
+function boxChecker(board, row, coloum) {
+
+    let startRow = Math.floor(row / 3) * 3;
+    let startCol = Math.floor(coloum / 3) * 3;
+
+    let blockSet = new Set()
+    for (let r = startRow; r < startRow + 3; r++) {
+        for (let c = startCol; c < startCol + 3; c++) {
+            if (board[r][c] !== '.') {
+                if (!blockSet.has(board[r][c]) && (board[r][c] >= 1 && board[r][c] <= 9)) {
+                    blockSet.add(board[r][c])
+                } else {
+                    return false
+                }
+            }
+        }
+    }
+    return true
+}
