74be4d60d5e7fcd644f8855dacbaa16cccfe289f
Madhuja Mitra <44411291+madhujamitra@users.noreply.github.com>
2024-11-02 19:19:07 -0700

    Time: 56 ms (82.57%) | Memory: 52.5 MB (23.49%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 1
+        for (let j = i; j < nums.length -1; j++) {
+
+            if (nums[i] === nums[j+ 1]) {
+                count++
+                if (count > 2) {
+                    nums.splice(j + 1, 1)
+                    j--
+                }
+            }
+        }
+    }
+
+    return nums.length
+
+};
